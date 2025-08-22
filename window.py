4eb7a5abfe0e2ece9d6f365e29dd9b132392b7e6
ModulandoStars <me@modulandostars.fyi>
2025-08-22 19:21:26 -0300

    attempting to make the listbox update when the list updates

--- window.py
+++ window.py
@@ -1,44 +1,83 @@
 import tkinter as tk
+import os
+
+
 
 root = tk.Tk()
 software_title = "A janela gamer"
 
-def HelloButton():
-    print("buceta")
-
-# ----------Botão-----------
-
-button = tk.Button(root,
-        text="oiaaaaaaaaaaaa",
-        height=1,
-        width=1,
-        command=HelloButton
-)
-
-button.pack(
-    ipadx=1,
-    ipady=1,
-)
-# --------------------------
-
-listaboa = ('pica', 'buceta', 'cu', 'boca')
-
-listBoxConvert = tk.Variable(value=listaboa)
-
-# ---------Lista------------
-lista = tk.Listbox(
-    root,
-    listvariable=listBoxConvert,
-    height=6
-)
-
-lista.pack(ipadx=10, ipady=15, fill=tk.BOTH)
-# --------------------------
-
 root.geometry('640x510')
 root.resizable(False, False)
 
+IntroductionText = tk.StringVar()
+IntroductionText.set("olá, bem vindo ao seletor de pastas aleatórias!")
+
+#selectedPath = input("sexo")
+
+
+#folderDirList = []
+
+#   def checkFolders(directory):
+#        print(directory + " foi o diretório")
+#       folderCheck = os.scandir(directory)
+#       for entry in folderCheck:
+#          if entry.is_dir():
+#                folderDirList.append(entry.name)
+
+
+textList = []
+variableList = tk.Variable(value=textList)
+
+
+
+text = tk.Label(
+    root,
+    anchor=tk.CENTER,
+    height=2,
+    textvariable=IntroductionText
+)
+text.pack(pady=15)
+
+dirAskBox = tk.Text(
+    root,
+    height=5,
+    width=10,
+    font=("Segoe UI", 12)
+)
+dirAskBox.insert(tk.END, "C:\\Windows")
+dirAskBox.pack()
+textBox = dirAskBox.get("1.0", tk.END)
+
+def inmendText(text):
+    textBox = dirAskBox.get("0.0", tk.END)
+    variableList = tk.Variable(value=textList)
+    textList.append(text)
+    print(textList)
+    root.update()
+
+doFunction = tk.Button(
+    root,
+    text="hello",
+    command= lambda: inmendText(textBox)
+)
+doFunction.pack(pady=5)
+
+
+
+
+folderList = tk.Listbox(
+    root,
+    bg="white",
+    width=50,
+    listvariable=variableList,
+    font=("Segoe UI", 12)
+)
+folderList.pack()
+
+
+
+
 root.title(software_title)
-root.configure(background="gray")
+root.configure(background="dark grey")
 
 root.mainloop()
